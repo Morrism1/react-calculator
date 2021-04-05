1509d583ad9167e2786ce03227aee24b62e5458c
Morrism1 <morrismontana0@gmail.com>
2021-04-05 19:47:34 +0200

    Import button panel

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import Display from './Display';
+import ButtonPanel from './ButtonPanel';
 import '../styles/App.css';
 
 function App() {
   return (
     <>
       <Display />
+      <ButtonPanel />
     </>
   );
 }
