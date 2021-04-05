8500c92a805fe8c01fb9ba2e91aeb8e9b5889bae
Morrism1 <morrismontana0@gmail.com>
2021-04-05 18:51:06 +0200

    Create components directory to house all my components

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import '../styles/App.css';
+
+function App() {
+  return (
+    <h1>
+      Calculator
+      <span role="img" aria-label="calculator-emoji">🧮</span>
+    </h1>
+  );
+}
+
+export default App;
