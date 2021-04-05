6a567c957fb63ba47739426b1da388b6b7a6b0c3
Morrism1 <morrismontana0@gmail.com>
2021-04-05 19:56:38 +0200

    Create the button component

---
+++ src/components/Button.js
@@ -0,0 +1,13 @@
+import PropTypes from 'prop-types';
+
+export default function Button({ name }) {
+  return (
+    <div>
+      {name}
+    </div>
+  );
+}
+
+Button.propTypes = {
+  name: PropTypes.string.isRequired,
+};
