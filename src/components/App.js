79e2050f58dab752a1f6e303f566c2a6e481bf28
Morrism1 <morrismontana0@gmail.com>
2021-04-07 01:53:57 +0200

    Make App component statefull and making it a class

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,35 @@
+import { Component } from 'react';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 import '../styles/App.css';
-// import calculate from '../logic/calculate';
 
-function App() {
-  return (
-    <table className="main-box" cellSpacing="0" cellPadding="0">
-      <Display />
-      <ButtonPanel />
-    </table>
-  );
+import calculate from '../logic/calculate';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      total: 0,
+      next: null,
+      operation: null,
+    };
+  }
+
+  handleClick(buttonName) {
+    const result = calculate(this.state, buttonName);
+    this.setState(result);
+  }
+
+  render() {
+    const { total, next } = this.state;
+    return (
+      <table className="main-box" cellSpacing="0" cellPadding="0">
+        <Display result={(next) || (total)} />
+        <ButtonPanel handleCkick={(name) => this.handleClick(name)} />
+      </table>
+    );
+  }
 }
 
 export default App;
