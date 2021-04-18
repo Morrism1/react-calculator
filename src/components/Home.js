6992d67f0f19743480cbd9205d44ede14e275509
Morrism1 <morrismontana0@gmail.com>
2021-04-18 22:31:19 +0200

    Add home component

---
+++ src/components/Home.js
@@ -0,0 +1,24 @@
+import Jumbotron from 'react-bootstrap/Jumbotron';
+
+export default function Home() {
+  return (
+    <>
+      <h1>Math Magician</h1>
+      <Jumbotron>
+        <h3>Welcome to our Page</h3>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+          Alias natus molestiae voluptatem
+          similique harum nemo
+          velit itaque a sequi hic laudantium, labore consectetur nihil exercitationem
+          voluptates assumenda impedit error
+          nesciunt!lorem
+          lore nesciunt!lorem Lorem ipsum dolor sit,
+          amet consectetur adipisicing elit. Commodi sapiente natus expedita,
+          ullam recusandae, voluptates modi at, culpa omnis animi nobis est soluta quibusdam.
+          Velit dolor esse pariatur at blanditiis?
+        </p>
+      </Jumbotron>
+    </>
+  );
+}
