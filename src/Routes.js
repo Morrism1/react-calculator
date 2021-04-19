a997c9fc4f46b2631f34089e02128ad1fc3e7b6c
Morrism1 <morrismontana0@gmail.com>
2021-04-19 11:59:18 +0200

    Add navbar to the app

--- src/Routes.js
+++ src/Routes.js
@@ -3,14 +3,18 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import App from './components/App';
 import Home from './components/Home';
 import Quote from './components/Quote';
+import NavBar from './components/NavBar';
 
 const Routes = () => (
   <BrowserRouter>
-    <Switch>
-      <Route path="/" component={Home} />
-      <Route path="/calculator" component={App} />
-      <Route path="/quote" component={Quote} />
-    </Switch>
+    <NavBar />
+    <div className="container mt-2" style={{ marginTop: 40 }}>
+      <Switch>
+        <Route exact path="/"><Home /></Route>
+        <Route path="/calculator"><App /></Route>
+        <Route path="/quote"><Quote /></Route>
+      </Switch>
+    </div>
   </BrowserRouter>
 );
 
