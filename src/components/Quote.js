348ea067a9e4973661a0f719a5490dc89b3e0180
Morrism1 <morrismontana0@gmail.com>
2021-04-19 12:01:44 +0200

    Add a quote in the component

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,5 +1,11 @@
 export default function Quote() {
   return (
-    null
+    <div>
+      <h1>Math Magician</h1>
+      <p>
+        Mathematics is not about numbers, equations, computations,
+        or algorithms: it is about understanding. –William Paul Thurston
+      </p>
+    </div>
   );
 }
